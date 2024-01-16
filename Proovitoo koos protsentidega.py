1a0c8b92873aadaf954089ed0829a31ccb43ddd1
AOrmisson <87547788+AOrmisson@users.noreply.github.com>
2024-01-16 16:45:16 +0200

    Proovitöö.py

--- Proovitoo koos protsentidega.py
+++ Proovitoo koos protsentidega.py
@@ -1,58 +1,73 @@
-import string
-import math
+"""Proovitöö"""
+import re
+import matplotlib.pyplot as plt
+from tabulate import tabulate
 
-filename = input("Enter a file name: ")
+def text_to_word_combinations(filename: str):
+    """
+    Write a program that takes input text and returns up to 10
+    most frequent letter compound with their percentage of occurrence presented as a table and as a bar chart.
+    """
+    with open(filename, "r") as file:  # Loeb antud failist teksti sisse.
+        text = file.read()
+        text = text.lower()  # Muudab teksti kõik tähed väiketähtedeks.
+    words = re.findall(r"\w{4,}", text)  # Leiab tekstist kõik tühikuga eraldatud sõnad mis on 4 või rohkem tähte pikad.
+    combinations = {}
+    for word in words:
+        for length in range(4, len(word) + 1):  # Tekitab vahemiku iga leitud sõna pikkuse kohta.
+            for item in range(len(word) - length + 1):  # Leiab sõna esimese tähe vastavalt sõna pikkusest.
+                if word[item: item + length] in combinations.keys():  # Kui sõna esineb sõnastikus, tõstetakse väärtust.
+                    combinations[word[item: item + length]] += 1
+                else:
+                    combinations[word[item: item + length]] = 1  # Kui sõna ei ole sõnastikus, lisatakse see sinna.
+    # Leiab kõikide sõnade koguarvu.
+    total_sum_of_words = sum(combinations.values())
 
-with open(filename, "r") as file:
-    text = file.read()
-    words = text.split()
-    table = str.maketrans("", "", string.punctuation)
-    stripped = [char.translate(table) for char in words]
-#Eemaldasin kirjavahemärgid tekstist
+    # Sorteerib elemendid vastavalt esinemisväärtusele.
+    sorted_items = sorted(combinations.items(), key=lambda x: x[1], reverse=True)
+    sorted_items = sorted_items[:10]
 
-Count = len(stripped)
-Percentage = Count *100
+    # Tabeli loomine:
+    table_headers = ['Täheühend', 'Esinemise arv', 'Esinemise protsent']  # Loob tabeli esimese rea
+    table_data = []
 
-word_dict = dict()
+    for word, count in sorted_items:  # Leiab informatsiooni iga täheühendi kohta.
+        percentage = (count / total_sum_of_words) * 100
+        table_data.append([word, count, f'{percentage:.2f}%'])
 
-for word in stripped:
-    if word in word_dict:
-        word_dict[word] = word_dict[word] + 1
-    else:
-        word_dict[word] = 1
-#Loendan kõik sõnad hulga poolest ja lisan nad dictionarisse
+    # Tabel konsoolis esitatult:
+    table_str = tabulate(table_data, headers=table_headers, tablefmt="pretty")
+    print(table_str)
 
-for word in tuple(word_dict.keys()):
-    if word.isdigit():
-        del word_dict[word]
-#Ei võimalda lisada nubmreid dictionarisse
+    plt.figure(figsize=(8, 4))
+    # Määrab tabelis kuvatava info ja vorminduse.
+    table = plt.table(cellText=table_data, colLabels=table_headers, cellLoc='center', loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.scale(1.2, 1.2)
+    plt.axis('off')  # Eemaldab ebavajaliku skaala.
+    plt.title('Sõnaühendite esinemise protsent failis')
+    plt.show()
 
-#print(sorted(zip(word_dict.values(), word_dict.keys())))
+    # Tulpdiagrammi loomine:
+    # Leiab informatsiooni iga täheühendi kohta.
+    words = [word for word, _ in sorted_items]
+    percentages = [(count / total_sum_of_words) * 100 for _, count in sorted_items]
+    max_percentage = max(percentages)  # Leiab kõige kõrgema esinemise protsendi.
+    max_percentage += 5
 
-from collections import Counter
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(words, percentages, color='blue')  # Määrab kuvatava info.
+    # Diagrammi eri osade pealkirjad:
+    plt.xlabel('Täheühendid')
+    plt.ylabel('Esinemise protsent')
+    plt.title('Sõnaühendite esinemise protsent failis')
+    plt.xticks(rotation=0, ha='right')
 
-C = Counter(word_dict)
-high = C.most_common(10)
-
-print("Top10 sõna")
-print("Nimi: Kogus")
-
-for i in high:
-    print(i[0],':',(i[1] / Count * 100).__format__(".2f"), "%")
-
-
-#Otsisin välja top10 enim esinevat sõna ja prindin need välja tabelina
-
-import matplotlib.pyplot as plot
-
-Top10 = dict(C.most_common(10))
-x = list(Top10.keys())
-y = list(Top10.values())
-
-plot.bar(x, y)
-plot.xticks(rotation=90)
-plot.xlabel("Sõnad")
-plot.ylabel("Kogus")
-plot.title("Top10 enimesinevat sõna")
-plot.show()
-#Loon tulpdiagrammi kasutades varemloodud most_common listi, keeran s6nad vertikaalselt, et k6ik mahuks diagrammile.
+    # Protsentide kuvamine tulpade tipus:
+    for bar, percentage in zip(bars, percentages):
+        plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 1,
+                 f'{percentage:.2f}%', ha='center', color='black')
+    plt.ylim(0, max_percentage)  # Pikendab tabelit 5% võrra.
+    plt.tight_layout()
+    plt.show()
